fa2854925dbf2a2d3a63060f06fb99a4f53ed524
Dan Birch <dbirch@jigsaw-research.co.uk>
2022-11-04 15:47:40 +0000

    adds grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container text-center">
     <div class="p-10">
-      <div class="justify-center flex items-center gap-3">
+      <div v-if="!finished"
+           class="justify-center flex items-center gap-3"
+      >
         <span v-for="item in selectedTimesTables">
           <input :id="item.name"
                  v-model="item.value"
@@ -13,10 +15,10 @@
       </div>
 
       <template v-if="totalQuestions">
-        <div v-if="currentIndex < totalQuestions -1"
+        <div v-if="!finished"
              class="justify-center flex items-center m-3 gap-2"
         >
-          <span>{{ flatListOfQuestions[currentIndex].sum }}</span>
+          <span class="sum">{{ flatListOfQuestions[currentIndex].sum }}</span>
           <span><input v-model="flatListOfQuestions[currentIndex].userAnswer"
                        type="text"
           ></span>
@@ -32,19 +34,34 @@
           >
             Next
           </button>
+          <button v-if="currentIndex === totalQuestions - 1"
+                  @click.prevent="finish"
+          >
+            Finish
+          </button>
         </div>
       </template>
     </div>
 
-
-
-
-    <div v-if="totalQuestions">
-      Correct answers: {{ correctAnswers.length }}
-      Wrong answers: {{ incorrectAnswers.length }}
-      Total questions: {{ totalQuestions }}
+    <div class="numberSquare grid grid-cols-10">
+      <template v-for="x in totalGrid">
+        <span v-for="item in grid"
+              class="dot"
+              :class="`dot_${isOdd(x)}`"
+        >&nbsp;</span>
+      </template>
     </div>
-    <button @click.prevent="reset">
+
+    <div v-if="totalQuestions"
+         class="m-5"
+    >
+      <div>Correct answers: {{ correctAnswers.length }}</div>
+      <div>Wrong answers: {{ incorrectAnswers.length }}</div>
+      <div>Total questions: {{ totalQuestions }}</div>
+    </div>
+    <button v-if="currentIndex > 0"
+            @click.prevent="reset"
+    >
       Start Again!
     </button>
   </div>
@@ -79,15 +96,25 @@ export default {
   data(){
     return {
       range: Array.from({ length: 11 }, (_, idx) => (idx + 2)),
-      grid: Array.from({ length: 7 }, (_, idx) => (idx + 1)),
       selectedTimesTables: buildSelectedTimesTables(),
       showAnswers: false,
+      finished: false,
       allowPrev: false,
       currentIndex: 0,
       flatListOfQuestions: []
     };
   },
   computed: {
+    grid(){
+      const selected = this.selectedTimesTables.filter(item => item.value);
+      const wrap = selected.length === 1 ? selected[0].timesTable : 10;
+      return Array.from({ length: wrap }, (_, idx) => (idx + 1));
+    },
+    totalGrid(){
+      const selected = this.selectedTimesTables.filter(item => item.value);
+      const wrap = selected.length === 1 ? selected[0].timesTable : 10;
+      return Array.from({ length: 100/wrap }, (_, idx) => (idx + 1));
+    },
     correctAnswers(){
       return this.flatListOfQuestions.filter((item, index) => item.answer === parseInt(item.userAnswer) && index < this.currentIndex);
     },
@@ -119,20 +146,47 @@ export default {
       this.currentIndex -= 1;
     },
     reset(){
-      //git remote add origin git@github.com:danbirch9000/maths.git
-
-      //git remote set-url origin git@github.com:danbirch9000/maths.git
       this.currentIndex = 0;
+      this.finished = false;
       this.selectedTimesTables = buildSelectedTimesTables();
+    },
+    isOdd(num){
+      return num % 2 === 0 ? 'row_a' : 'row_b';
+    },
+    finish(){
+      this.finished = true;
+      this.goNext();
     }
   }
 };
 </script>
  <style lang="scss" scoped>
-  input {
+  input[type='text'] {
     @apply p-2 rounded border border-gray-300;
+    font-size: 50px;
+    width: 120px;
+    color: #506266;
   }
   button {
     @apply bg-gray-400 p-2 rounded;
+  }
+  .sum {
+    color: #506266;
+    font-size: 50px;
+  }
+  .numberSquare {
+    @apply m-auto;
+    width: 400px;
+    span.dot {
+      @apply m-3 block rounded-full;
+      width: 12px;
+      height: 12px;
+      &.dot_row_b {
+        background-color: #506266;
+      }
+      &.dot_row_a {
+        background-color: #BDE038;
+      }
+    }
   }
  </style>
